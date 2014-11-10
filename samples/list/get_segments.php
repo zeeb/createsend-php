2115aeee8d3299b918cf0d536de1f2bbfc24ee77
Rosie Harvey <rosie.harvey@gmail.com>
2014-11-10 15:37:12 +0000

    Update get_segments.php
    Successful response comment refers to custom fields, not segment!

--- samples/list/get_segments.php
+++ samples/list/get_segments.php
@@ -11,7 +11,7 @@ $result = $wrap->get_segments();
 
 echo "Result of GET /api/v3.1/lists/{ID}/segments\n<br />";
 if($result->was_successful()) {
-    echo "Got custom fields\n<br /><pre>";
+    echo "Got segments\n<br /><pre>";
     var_dump($result->response);
 } else {
     echo 'Failed with code '.$result->http_status_code."\n<br /><pre>";
